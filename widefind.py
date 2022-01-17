56a637992ba8cd3a6bf27bea6a87305f8cf8db55
martinaskolin <80485419+martinaskolin@users.noreply.github.com>
2022-01-17 22:41:23 +0100

    Update widefind.py

--- widefind.py
+++ widefind.py
@@ -1,14 +1,12 @@
-from threading import Thread
-from transform import Transform, Vector3
+from transform import Vector3
 import paho.mqtt.client as mqtt
 import json
 
 
-class WideFind(Thread):
+class WideFind:
+    """Used for connecting to a WideFind sensor."""
 
     def __init__(self, url: str, port: int):
-        Thread.__init__(self)
-
         # MQTT IP for WideFind
         self.broker_url = url
         self.broker_port = port
@@ -19,6 +17,7 @@ class WideFind(Thread):
         self.__client.on_message = self.__on_message
 
         # WideFind variables
+        self.trackers = {}
         self.debug = False
 
     def run(self, debug=False) -> None:
@@ -30,32 +29,28 @@ class WideFind(Thread):
 
     def __on_connect(self, client, userdata, flags, rc, properties=None):
         if self.debug:
-            print("Connected to " + self.broker_url + ":" + self.broker_port + " with reason code: " + rc)
-            # TODO: Remove 'reason code' rc if it is useless
+            print("Connected to " + self.broker_url + ":" + self.broker_port)
 
     def __on_message(self, client, userdata, message):
+        # Decode message and put into list
         mqtt_message_str = message.payload.decode()
         mqtt_message_json = json.loads(mqtt_message_str)
         mqtt_message_list = mqtt_message_json["message"].split(',')
 
-        # TODO: Clean up unnecessary code, added for now to skip look up
+        # Update tracker information
+        tracker_id = mqtt_message_list[0][7:]
         position = Vector3()
         position.x = mqtt_message_list[2]
         position.y = mqtt_message_list[3]
         position.z = mqtt_message_list[4]
 
-        tracker_id = mqtt_message_list[0][7:]
+        self.trackers[tracker_id] = position
 
-        # TODO: Decide if tracker_id is unique. If it is add it to list of trackers else update already existing one
+        if self.debug:
+            print("Tracker with id: " + tracker_id + ", currently positioned at " + position)
+
+        # TODO: Test if threading is really necessary...
         # TODO: Add observer design pattern so that anyone can subscribe to WideFind events
-        # TODO: Add documentation to public functions and class
         # https://refactoring.guru/design-patterns/observer/python/example
 
         # The rest of the code provided is probably for camera look at coordinates!
-
-
-class Tracker:
-
-    def __init__(self, tracker_id: str):
-        self.tracker_id = tracker_id
-        self.transform = Transform()
