2c244a18c3bfad006180821f1d526264ae904ff1
lundsak <lundmarkisak@gmail.com>
2022-02-07 14:59:36 +0100

    Camera calibration

--- test.py
+++ test.py
@@ -9,13 +9,21 @@ from MVC_interface.view import View
 src = "rtsp://130.240.105.144:554/mediainput/h264/stream_1"
 # src = 0 # Face cam for debug
 
-camera_bedroom_pos = numpy.array([1162, 3335, 2326])
-camera_bedroom_zero = numpy.array([133, 3628, 2193])
-camera_bedroom_floor = numpy.array([632, 3378, 597])
+# camera_bedroom_pos = numpy.array([1162, 3335, 2326])
+# camera_bedroom_zero = numpy.array([133, 3628, 2193])
+# camera_bedroom_floor = numpy.array([632, 3378, 597])
 
-camera_kitchen_pos = numpy.array([2873, -2602, 2186])
-camera_kitchen_zero = numpy.array([3413, -2722, 2284])
-camera_kitchen_floor = numpy.array([2694, -2722, 193])
+camera_bedroom_pos = numpy.array([619, 3935, 2600])
+camera_bedroom_zero = numpy.array([-765, 4112, 2878])
+camera_bedroom_floor = numpy.array([531, 3377, 331])
+
+# camera_kitchen_pos = numpy.array([2873, -2602, 2186])
+# camera_kitchen_zero = numpy.array([3413, -2722, 2284])
+# camera_kitchen_floor = numpy.array([2694, -2722, 193])
+
+camera_kitchen_pos = numpy.array([2815, -2606, 2005])
+camera_kitchen_zero = numpy.array([4403, -2808, 2436])
+camera_kitchen_floor = numpy.array([2926, -2518, 44])
 
 cam = HDIntegratedCamera("http://130.240.105.144/cgi-bin/aw_ptz?cmd=%23")
 widefind = wf.WideFind("130.240.74.55", 1883)
