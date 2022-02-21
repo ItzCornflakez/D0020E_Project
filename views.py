166569df05866df873b951161b97a5858e0ac977
ItzCornflakez <dikesalex@gmail.com>
2022-02-21 13:12:58 +0100

    simple test page with buttons that have functionality

--- views.py
+++ views.py
@@ -4,5 +4,29 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template('/index.html')
-    
+    return render_template('/test.html')
+
+@views.route('/rotate')
+def rotate():
+    print ("rotate")
+    return ("nothing")
+
+@views.route('/follow')
+def follow():
+    print ("follow")
+    return ("nothing")
+
+@views.route('/look')
+def look():
+    print ("look")
+    return ("nothing")
+
+@views.route('/switch')
+def switch():
+    print ("switch")
+    return ("nothing")
+
+@views.route('/disconnect')
+def disconnect():
+    print ("disconnect")
+    return ("nothing")
