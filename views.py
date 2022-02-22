378ff47ac2acfd869f1b24d7705fe42eb17413b8
lundsak <lundmarkisak@gmail.com>
2022-02-22 16:56:20 +0100

    Started to add some WideFind functionality

--- views.py
+++ views.py
@@ -1,12 +1,24 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, Response, make_response
 from test_controller import Controller
+import widefind as wf
 
 views = Blueprint('views', __name__)
 controller = Controller()
 
+try: 
+    widefind = wf.WideFind("130.240.74.55", 1883)
+    widefind.run("ltu-system/#", False)
+except:
+    raise RuntimeError('Could not connect to WideFind')
+else:
+    print("WideFind connected")
+
+widefind.attach(controller)
+
+
 @views.route('/')
 def home():
-    return render_template('/test.html')
+    return render_template('/index.html', widefindTrackers = widefind.trackers) #LAST PART NOT MVC
     
 @views.route('/rotate', methods=['POST'])
 def rotate():
@@ -35,3 +47,20 @@ def switch():
 def disconnect():
     print ("disconnect")
     return ("nothing")
+
+
+
+#WideFind stuff( NOT MVC)
+@views.route("/getWidefind")
+def getWidefind():
+    response = widefind.trackers
+    return Response(str(response))
+
+@views.route("/getWidefindCoordinates")
+@views.route("/getWidefindCoordinates/<wfID>")
+def getWidefindCoordinates(wfID):
+    response = widefind.trackers.get(wfID)
+    print(response)
+    return Response(str(response))
+
+
