af4256b187e713231c1d08a495016f4c36528a95
Alexander Österberg <dikesalex@gmail.com>
2022-03-07 08:41:22 +0100

    correct transforms for each room

--- test_controller.py
+++ test_controller.py
@@ -12,16 +12,16 @@ class Controller(Observer):
 
         self.src = "http://130.240.105.144/cgi-bin/mjpeg?resolution=1920x1080&amp;framerate=5&amp;quality=1"
 
-        camera_bedroom_pos = numpy.array([619, 3935, 2600])
-        camera_bedroom_zero = numpy.array([-765, 4112, 2878])
-        camera_bedroom_floor = numpy.array([531, 3377, 331])
+        self.camera_bedroom_pos = numpy.array([619, 3935, 2600])
+        self.camera_bedroom_zero = numpy.array([-765, 4112, 2878])
+        self.camera_bedroom_floor = numpy.array([531, 3377, 331])
 
-        camera_kitchen_pos = numpy.array([2873, -2602, 2186])
-        camera_kitchen_zero = numpy.array([3413, -2722, 2284])
-        camera_kitchen_floor = numpy.array([2694, -2722, 193])
+        self.camera_kitchen_pos = numpy.array([2873, -2602, 2186])
+        self.camera_kitchen_zero = numpy.array([3413, -2722, 2284])
+        self.camera_kitchen_floor = numpy.array([2694, -2722, 193])
 
         self.cam = HDIntegratedCamera("http://130.240.105.144/cgi-bin/aw_ptz?cmd=%23")
-        self.cam_trans = wf.Transform(camera_kitchen_pos, camera_kitchen_zero, camera_kitchen_floor)
+        self.cam_trans = wf.Transform(self.camera_kitchen_pos, self.camera_kitchen_zero, self.camera_kitchen_floor)
 
         self.rotate(210, 140)
 
@@ -50,8 +50,10 @@ class Controller(Observer):
     def switchCam(self, cam):
         if(cam == "Kitchen"):
             self.cam = HDIntegratedCamera("http://130.240.105.144/cgi-bin/aw_ptz?cmd=%23")
+            self.cam_trans = wf.Transform(self.camera_kitchen_pos, self.camera_kitchen_zero, self.camera_kitchen_floor)
         if(cam == "Bedroom"):
             self.cam = HDIntegratedCamera("http://130.240.105.145/cgi-bin/aw_ptz?cmd=%23")
+            self.cam_trans = wf.Transform(self.camera_bedroom_pos, self.camera_bedroom_zero, self.camera_bedroom_floor)
 
     def up(self):
         self.cam.rotate(self.cam.get_current_yaw(), self.cam.get_current_pitch() + self.rot_amount)
