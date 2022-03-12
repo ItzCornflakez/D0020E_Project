f1d4462607d32395759c129e919097cbb7d67467
ItzCornflakez <dikesalex@gmail.com>
2022-03-12 10:31:27 +0100

    no open web_browser

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from __init__ import create_app
-import webbrowser
-from threading import Timer
 
 app = create_app()
 
-Timer(1, webbrowser.open_new('http://127.0.0.1:5000/')).start();
 app.run(debug=True)
 
 
